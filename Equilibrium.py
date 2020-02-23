f6ed77c47d5adea90edc949a8831bfcf9a9a33ae
sebasfalcken <sebastian.falckenheiner@gmail.com>
2020-02-23 11:56:16 +0100

    Create Equilibrium.py

---
+++ Equilibrium.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+C_a     = 0.484             #[m]
+l_a     = 1.691             #[m]
+x_1     = 0.149             #[m]
+x_2     = 0.554             #[m]
+x_3     = 1.541             #[m]
+x_a     = 27.2              #[m]
+h_a     = 17.3              #[m]
+d_1     = 0.00681           #[m]
+theta   = 26*np.pi()/180    #[rad]
+P       = 37.9              #[kN]
+
+
+
