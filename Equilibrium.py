4d1669c150770a8d768976c6a3fd491be0418c2b
sebasfalcken <sebastian.falckenheiner@gmail.com>
2020-02-24 17:32:55 +0100

    Update Equilibrium.py

--- Equilibrium.py
+++ Equilibrium.py
@@ -37,14 +37,40 @@ int4    = 2                         #[N*m^3]
 #- The change in theta from actuator I to actuator II is negligible for the P components calculation
 
 def dist(x):
-    d   = np.array([x-x_1, x-x_2+x_a/2, x-x_2, x-x_3, x-x_1, x-x_2, x-x_3, 0, 0, 0, 0, 0, x-x_2-x_a/2, 1])#14 elements
-    tf  = d>=0
-    return d*tf
+    return np.array([x-x_1, x-x_2+x_a/2, x-x_2, x-x_3, x-x_1, x-x_2, x-x_3, 0, 0, 0, 0, 0, x-x_2-x_a/2, 1])#14 elements
+    #This will return the Macaulay term for each of the variables according to an x value. 
 
-def T(x):
-    T_m = np.array([0, h_a/2*np.cos(theta)-(h_a/2-z_tilde)*np.sin(theta), 0, 0, z_tilde, z_tilde, z_tilde, 0, 0, 0, 0, 0,-(h_a/2*np.cos(theta)-(h_a/2-z_tilde)*np.sin(theta)), -1])
-    
-    
+def T_t(x):
+    T_m     = np.array([0, h_a/2*np.cos(theta)-(h_a/2-z_tilde)*np.sin(theta), 0, 0, z_tilde, z_tilde, z_tilde, 0, 0, 0, 0, 0,-(h_a/2*np.cos(theta)-(h_a/2-z_tilde)*np.sin(theta))*P, -1])
+    T_f     = (dist(x)>=0)*T_m
+    T_f[12] = T_f[12]+T_f[13]
+    return T_f[:-1]
+
+def M_y_t(x):
+    M_ym    = np.array([-1, -np.cos(theta), -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, -np.cos(theta)*P, 0])
+    M_yf    = dist(x)*(dist(x)>=0)*M_ym
+    M_yf[12]= M_yf[12]+M_yf[13]
+    return M_yf[:-1]
+
+def M_z_t(x):
+    M_zm    = np.array([0, np.sin(theta), 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, P*np.sin(theta), 1])
+    M_zf    = dist(x)*(dist(x)>=0)*M_zm
+    M_zf[12]= M_zf[12]+M_zf[13]
+    return M_zf[:-1]
+
+def S_y_t(x):
+    S_ym    = np.array([0, np.sin(theta), 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, np.sin(theta)*P, 1])
+    S_yf    = (dist(x)>=0)*S_ym
+    S_yf[12]= S_yf[12]+S_yf[13]
+    return S_yf[:-1]
+
+def S_z_t(x):
+    S_zm    = np.array([-1, -np.cos(theta), -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, -P*np.cos(theta), 0])
+    S_zf    = (dist(x)>=0)*S_zm
+    S_zf[12]= S_zf[12]+S_zf[13]
+    return S_zf[:-1]
+
+'''
 M_y     = np.array([-l_a+x_1, -np.cos(theta)*(l_a-x_2+x_a/2), -l_a+x_2, -l_a+x_3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 M_y_r   = -P*np.cos(theta)*(l_a-x_2-x_a/2)
 
@@ -60,6 +86,7 @@ S_z     = np.array([-1, -np.cos(theta), -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 S_z_r   = -P*np.cos(theta)
 
 T       = np.array([0, h_a/2*np.cos(theta)-(h_a/2-z_tilde)*np.sin(theta), 0, 0, z_tilde, z_tilde, z_tilde, 0, 0, 0, 0, 0, 0, 0])
+'''
 
 #So here I am puting in vector form the deflection and rotation equations of y, z and theta.
 #There will be two additional values compared to the ones that I already put P and the integral. 
@@ -110,9 +137,11 @@ BC_wx3      = BC_wx3[:12]
 
 BC_wxac1    = w_def(x_2-x_a/2)
 BC_wxac1_r  = BC_wxac1[-1]
-BC_wxac1    = BC_wx3[:12]
+BC_wxac1    = BC_wxac1[:12]
 
 #Now, the equations will be put in a matrix and will be solved :) :) !!!
 
-A = np.array([M_y, M_z,S_y,S_z,T])
+A = np.array([M_y_t(l_a)[:-1], M_z_t(l_a)[:-1], S_y_t(l_a)[:-1], S_z_t(l_a)[:-1], T_t(l_a)[:-1], BC_vx1, BC_vx2, BC_vx3, BC_wx1, BC_wx2, BC_wx3, BC_wxac1])
+bf = np.array([M_y_t(l_a)[-1], M_z_t(l_a)[-1], S_y_t(l_a)[-1], S_z_t(l_a)[-1], T_t(l_a)[-1], BC_vx1_r, BC_vx2_r, BC_vx3_r, BC_wx1_r, BC_wx2_r, BC_wx3_r, BC_wxac1_r])
 
+print(np.linalg.solve(A,bf))
