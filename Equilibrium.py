32a875f7f3d4e55fe6d9382ae9140cf09fea1d4c
sebasfalcken <sebastian.falckenheiner@gmail.com>
2020-02-27 15:37:52 +0100

    Equilibrium done

--- Equilibrium.py
+++ Equilibrium.py
@@ -75,22 +75,22 @@ def th_rot_t(x):
     return (th_a[:12], np.sum(th_a[-2:]))
 
 BC1     = v_def_t(x_1)[0]+th_rot_t(x_1)[0]*(z_tilde+h_a/2)
-BC_1_r  = v_def_t(x_1)[1]+th_rot_t(x_1)[1]*(z_tilde+h_a/2) + d_1
+BC_1_r  = v_def_t(x_1)[1]+th_rot_t(x_1)[1]*(z_tilde+h_a/2) + d_1*np.cos(theta)
 
 BC2     = v_def_t(x_2)[0]+th_rot_t(x_2)[0]*(z_tilde+h_a/2)
 BC_2_r  = v_def_t(x_2)[1]+th_rot_t(x_2)[1]*(z_tilde+h_a/2)
 
 BC3     = v_def_t(x_3)[0]+th_rot_t(x_3)[0]*(z_tilde+h_a/2)
-BC_3_r  = v_def_t(x_3)[1]+th_rot_t(x_3)[1]*(z_tilde+h_a/2) +d_3
+BC_3_r  = v_def_t(x_3)[1]+th_rot_t(x_3)[1]*(z_tilde+h_a/2) +d_3*np.cos(theta)
 
 BC4     = w_def_t(x_1)[0]
-BC_4_r  = w_def_t(x_1)[1]
+BC_4_r  = w_def_t(x_1)[1]-d_1*np.sin(theta)
 
 BC5     = w_def_t(x_2)[0]
 BC_5_r  = w_def_t(x_2)[1]
 
 BC6     = w_def_t(x_3)[0]
-BC_6_r  = w_def_t(x_3)[1]
+BC_6_r  = w_def_t(x_3)[1]-d_3*np.sin(theta)
 
 BC7     = w_def_t(x_2-x_a/2)[0]*np.cos(theta)+v_def_t(x_2-x_a/2)[0]*np.sin(theta)+th_rot_t(x_2-x_a/2)[0]*z_tilde*np.sin(theta)
 BC_7_r  = w_def_t(x_2-x_a/2)[1]*np.cos(theta)+v_def_t(x_2-x_a/2)[1]*np.sin(theta)+th_rot_t(x_2-x_a/2)[1]*z_tilde*np.sin(theta)
@@ -131,6 +131,3 @@ def w_def(x):
 def th_rot(x):
     th= np.array([0,h_a/2*np.cos(theta)+z_tilde*np.sin(theta),0,0,z_tilde+h_a/2,z_tilde+h_a/2,z_tilde+h_a/2,0,0,0,0,0, -P*(h_a/2*np.cos(theta)+z_tilde*np.sin(theta)), Aerodynamic_Load.tau2(x)*1000])
     return np.sum(th/G/J*(dist1(x)>=0)*dist1(x)*R1)+R1[11]
-
-print(BC4)
-print(BC_4_r)
